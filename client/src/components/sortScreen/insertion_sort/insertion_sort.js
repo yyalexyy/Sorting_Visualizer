f40ae791eaac28e434d1b23b1548a375fb55e782
yyalexyy <ajuan@terpmail.umd.edu>
2021-11-21 00:52:54 -0500

    add other skeleton sorting files

---
+++ client/src/components/sortScreen/insertion_sort/insertion_sort.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class InsertionSort extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    render(){
+        return(
+            <div>
+                <h1>In InsertionSort</h1>
+            </div>
+        )
+    }
+
+}
+
+export default InsertionSort;
