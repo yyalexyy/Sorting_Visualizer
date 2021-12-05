0db0f4cba8ad3cc58480412d599e810f51e28b67
yyalexyy <ajuan@terpmail.umd.edu>
2021-12-04 23:43:18 -0500

    implement bubble sort

--- client/src/components/sortScreen/algorithms/bubble_sort.js
+++ client/src/components/sortScreen/algorithms/bubble_sort.js
@@ -1,52 +1,20 @@
-import React, { Component } from 'react';
-import '../bars.css';
-import '../bars_container.css';
+import { swap } from '../helper/swap.js';
 
 
-class BubbleSort extends Component {
-    constructor(props) {
-        super(props);
+export const bubble_sort = async(array) => {
+    let n = array.length;
+    let moves = [];
 
-        console.log(this.props.contentArray);
-    }
-
-    async swap(arr, val1, val2) {
-        var temp = arr[val1];
-        arr[val1] = arr[val2];
-        arr[val2] = temp;
-    }
-
-    bubble_sort(arr) {
-        var n = arr.length;
-        var i, j;
-        
-        for(i = 0; i < n-1; i++) {
-            for(j = 0; j < n-i-1; j++) {
-                if(arr[j] > arr[j+1]) {
-                    this.swap(arr, j, j+1);
-                }
+    for(let i = 0; i < n-1; i++) {
+        for(let j = 0; j < n-i-1; j++) {
+            if(arr[j] > arr[j+1]) {
+                await swap(arr, j, j+1);
+                moves.push([j, j+1, true]);
+            } else {
+                moves.push([j, j+1, false]);
             }
         }
     }
 
-    render(){
-        console.log("IN bubble_sort.js");
-
-        var videos_bars = this.props.contentArray.map((value) => 
-            <li className='bars' id={value} key={value} style={{width : value}}>
-                {value}
-            </li>
-        );
-
-        return(
-            <div>
-                <ul className='bars_container'>
-                    {videos_bars}
-                </ul>
-            </div>
-        )
-    }
-
+    return moves;
 }
-
-export default BubbleSort;
