1959c345b32f45c8a837e5a9f55b72776a4cfe05
yyalexyy <ajuan@terpmail.umd.edu>
2022-01-20 21:54:31 -0500

    reverse bubble and insertion sort order results

--- client/src/components/sortScreen/algorithms/bubbleSort.js
+++ client/src/components/sortScreen/algorithms/bubbleSort.js
@@ -6,7 +6,7 @@ export const bubbleSort = async(array) => {
 
     for(let i = 0; i < n-1; i++) {
         for(let j = 0; j < n-i-1; j++) {
-            if(array[j] > array[j+1]) {
+            if(array[j] < array[j+1]) {
                 await swap(array, j, j+1);
                 moves.push([j, j+1, true]);
             } else {
